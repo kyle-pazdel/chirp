937147090bdbabc5b6a34fffe2d169180383dd7d
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-06-21 15:44:08 -0700

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,11 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   const user = useUser();
+
+  const {data} = api.posts.getAll.useQuery();
+  
 
   return (
     <>
@@ -19,7 +21,11 @@ export default function Home() {
         <div>
           {!user.isSignedIn && <SignInButton/>}{!!user.isSignedIn && <SignOutButton/>}
         </div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
